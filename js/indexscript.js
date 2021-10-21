77895823edfd7197a6a6cd099f5ee46afd471c3c
yassminetissaoui <yassminetissaoui@protonmail.com>
2021-10-21 03:00:53 +0100

    f

---
+++ js/indexscript.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("figure").mouseenter(function(){
+     
+       $(this).parent().css("color", "#e61c60"); 
+    }).mouseleave(function(){
+     
+       $(this).parent().css("color", "white");        
+    });
+});
