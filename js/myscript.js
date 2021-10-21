4d8533a5919dec0adc9eaacf2979e6eac9bd7d72
yassminetissaoui <87478491+yassminetissaoui@users.noreply.github.com>
2021-10-21 07:14:04 +0100

    fin

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function () {
     $("figure").mouseenter(function(){
      
@@ -8,14 +7,3 @@ $(document).ready(function () {
        $(this).parent().css("color", "black");        
     });
 });
-=======
-$(document).ready(function () {
-    $("figure").mouseenter(function(){
-     
-       $(this).parent().css("color", "#e61c60"); 
-    }).mouseleave(function(){
-     
-       $(this).parent().css("color", "black");        
-    });
-});
->>>>>>> efdf9939bae00e7905a0d97449dacb7002c67fc7
