a127112a3474f9ed0227aa6c37acc4cd790d4d74
yassminetissaoui <87478491+yassminetissaoui@users.noreply.github.com>
2021-10-21 08:28:24 +0100

    commit

--- js/indexscript.js
+++ js/indexscript.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $(document).ready(function () {
     $("figure").mouseenter(function(){
      
@@ -7,3 +8,14 @@ $(document).ready(function () {
        $(this).parent().css("color", "white");        
     });
 });
+=======
+$(document).ready(function () {
+    $("figure").mouseenter(function(){
+     
+       $(this).parent().css("color", "#e61c60"); 
+    }).mouseleave(function(){
+     
+       $(this).parent().css("color", "white");        
+    });
+});
+>>>>>>> ec9b50ffa7da20abff0a5f3bdccbab8873922669
