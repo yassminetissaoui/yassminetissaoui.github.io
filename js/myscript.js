fd0c04201836f6a030843b7a65e1161ebca75a8d
yassminetissaoui <87478491+yassminetissaoui@users.noreply.github.com>
2021-10-21 08:57:16 +0100

    fix

--- js/myscript.js
+++ js/myscript.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(function () {
     $("figure").mouseenter(function(){
      
@@ -8,14 +7,3 @@ $(document).ready(function () {
        $(this).parent().css("color", "black");        
     });
 });
-=======
-$(document).ready(function () {
-    $("figure").mouseenter(function(){
-     
-       $(this).parent().css("color", "#e61c60"); 
-    }).mouseleave(function(){
-     
-       $(this).parent().css("color", "black");        
-    });
-});
->>>>>>> ec9b50ffa7da20abff0a5f3bdccbab8873922669
