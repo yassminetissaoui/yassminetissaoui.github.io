8f50dcad3e4fcb8d730da934af4d9c2403a8fde3
yassminetissaoui <yassminetissaoui@protonmail.com>
2022-01-23 16:21:33 +0100

    dd
    dd

--- js/indexscript.js
+++ js/indexscript.js
@@ -7,10 +7,7 @@ $(document).ready(function () {
        $(this).parent().css("color", "white");        
     });
 });
-<<<<<<< HEAD
 
 $(function() {
             $( ".draggable" ).draggable();
          });
-=======
->>>>>>> 089eba184dc07c5f1b7be7861054194700042f48
